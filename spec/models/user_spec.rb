7e6d5d614a6289099e7f9e56670c0bb80f72dfd1
Alan Locke <jlocke2288@gmail.com>
2013-09-05 14:21:14 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
  
@@ -74,6 +74,12 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
